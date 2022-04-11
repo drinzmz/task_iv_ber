2370ccc5053d9adc6192a5fc7f279a3a8197922b
mzielinski@ahe.lodz.pl <mzielinski@ahe.lodz.pl>
2022-04-11 08:54:28 +0200

    hammingDistance

--- task_iv_ber/main.cpp
+++ task_iv_ber/main.cpp
@@ -1,5 +1,7 @@
 ﻿#include "pch.h"
 
+uint8_t hammingDistance(uint8_t n1, uint8_t n2);
+
 int main(int argc, char * argv[])
 {
     std::cout << "BER Calc v1.0" << std::endl;
@@ -9,5 +11,19 @@ int main(int argc, char * argv[])
         std::cout << "argv[" << iter << "] =" << argv[iter] << std::endl;
     }
 
+    std::cout << (int)hammingDistance(0xFF, 0x01) << std::endl;
+
     return 0;
+}
+
+uint8_t hammingDistance(uint8_t n1, uint8_t n2)
+{
+    uint8_t x = n1 ^ n2; // XOR
+    uint8_t setBits = 0;
+    while (x > 0)
+    {
+        setBits += x & 1;
+        x >>= 1;
+    }
+    return setBits;
 }
