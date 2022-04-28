812c950e77c08d13e11f6ef583e86a62d40d16d7
ArtSzy93 <szyplakowski.a@gmail.com>
2022-04-28 21:47:24 +0200

    - Dodanie pliku funkcje.cpp wraz z nagłówkiem. - Przeniesienie funkcji hamingDistance z main.cpp do funkcje.cpp - Wstępne pomysły, co dalej..

---
+++ task_iv_ber/funkcje.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <iostream>
+
+
+uint8_t hammingDistance(uint8_t n1, uint8_t n2); // Funkcja z przyk³adu
--- task_iv_ber/main.cpp
+++ task_iv_ber/main.cpp
@@ -1,4 +1,5 @@
 ﻿#include "pch.h"
+#include "funkcje.h"
 
 uint8_t hammingDistance(uint8_t n1, uint8_t n2);
 
@@ -16,14 +17,3 @@ int main(int argc, char * argv[])
     return 0;
 }
 
-uint8_t hammingDistance(uint8_t n1, uint8_t n2)
-{
-    uint8_t x = n1 ^ n2; // XOR
-    uint8_t setBits = 0;
-    while (x > 0)
-    {
-        setBits += x & 1;
-        x >>= 1;
-    }
-    return setBits;
-}
