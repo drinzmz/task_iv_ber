9181400f8f359afe66f5ee457c5dbfcbbebee335
mzielinski@ahe.lodz.pl <mzielinski@ahe.lodz.pl>
2022-05-06 09:29:03 +0200

    final project example

--- task_iv_ber/main.cpp
+++ task_iv_ber/main.cpp
@@ -1,23 +1,56 @@
 ﻿#include "pch.h"
 
+struct berResults
+{
+    double tot; //total number of bits
+    double err; //error bits (different bits)
+    float ber;  //ber calc result
+    clock_t t1; //calc start time
+    clock_t t2; //calc stop time
+};
+
 uint8_t hammingDistance(uint8_t n1, uint8_t n2);
 void createFile1(const std::string name, const int count, const char value);
+berResults calculateBer(std::string fpath1, std::string fpath2);
+void printResult(berResults results);
 
 int main(int argc, char * argv[])
 {
-    std::cout << "BER Calc v1.0" << std::endl;
-    openLog("log.log");
-    saveLog("Program uruchomiono poprawnie");
-    closeLog();
+    std::string fpath1; //path to file 1
+    std::string fpath2; //path to file 2
+    berResults results; 
 
-    std::cout << "argc =  : " << argc << std::endl; 
-    int iter = 0;
-    for (iter = 0; iter < argc; iter++) {
-        std::cout << "argv[" << iter << "] =" << argv[iter] << std::endl;
+    openLog("log.log"); //create log.log file for logging purposes
+    if (argc != 3) //check the number of arguments
+    {
+        saveLog("User did not provide path to the files.");
+        saveLog("Creating the test files for user...");
+        //test 1
+        createFile1("test1_file1.bin", 100, 0xFF); //1111 1111
+        createFile1("test1_file2.bin", 100, 0xFE); //1111 1110
+        //test 2
+        
+        /*
+            missing function to change 10 bits in the file
+        */
+
+        //test 3
+        //createFile1("test3_file1.bin",400000000,0x55); 
+        //createFile1("test3_file2.bin",400000000,0x50);
+        saveLog("Test files are prepared");
+        saveLog("Re-run with correct arguments ie: ./task_iv_ber.exe test1_file1.bin test1_file2.bin");
+    }
+    else //arguments are passed correctly
+    {
+        fpath1 = argv[1];
+        fpath2 = argv[2];
+
+        saveLog("Processing test files");
+        results = calculateBer(fpath1, fpath2);
+        printResult(results);
     }
 
-    std::cout << (int)hammingDistance(0xFF, 0x01) << std::endl;
-    createFile1("test1_file1.bin", 100, 0x55);
+    closeLog();
     return 0;
 }
 
@@ -42,4 +75,49 @@ void createFile1(const std::string name, const int count, const char value)
         f.write((char*)&value,1);
     }
     f.close();
+}
+
+berResults calculateBer(std::string fpath1, std::string fpath2)
+{
+    std::fstream f1, f2; //handlers to files
+    berResults results;
+    results.t1 = 0;
+    results.t2 = 0;
+    results.ber = 0;
+    results.err = 0;
+    results.tot = 0;
+
+    saveLog("Calculating BER...");
+    f1.open(fpath1.c_str(), std::ios::binary | std::ios::in);
+    f2.open(fpath2.c_str(), std::ios::binary | std::ios::in);
+    char a = 0x00;
+    char b = 0x00;
+    results.t1 = clock();
+
+    while (!f1.eof())
+    {
+        f1 >> a; //read 1 char from file 1
+        f2 >> b; //read 1 char from file 2
+        if (!f1.eof()) // till the end of the 1st file
+        {
+            results.err += hammingDistance(a, b); //add to the .err the number of different bits
+            results.tot += 8; //add to the .tot the number of compared bits
+        }
+    }
+
+    results.ber = (float)results.err / results.tot; // calculate ber
+    results.t2 = clock(); 
+    saveLog("BER calculations are done");
+    return results; //return structure with all results
+}
+
+void printResult(berResults results)
+{
+    std::stringstream message;
+    message << "Results are: " << std::endl;
+    message << "BER: " << results.ber << std::endl;
+    message << "Tot: " << results.tot << std::endl;
+    message << "Err: " << results.err << std::endl;
+    message << "Calc time: " << ((float)results.t2 - results.t1) / CLOCKS_PER_SEC << " sec " << std::endl;
+    saveLog(message.str());
 }
